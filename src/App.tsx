9f1ac1c7bd29fa63f0d80f492604e6bde0f32e1d
youjin-10 <105618795+youjin-10@users.noreply.github.com>
2024-07-13 20:55:01 +0900

    fix: mui theme provider with poppins font

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,34 @@
 import FleetDashboard from "./components/FleetDashboard";
+import { Container, ThemeProvider, createTheme } from "@mui/material";
 
 import "@fontsource/poppins/300.css";
 import "@fontsource/poppins/400.css";
 import "@fontsource/poppins/500.css";
 import "@fontsource/poppins/600.css";
 
+const theme = createTheme({
+  typography: {
+    fontFamily: ["poppins", "Roboto"].join(","),
+  },
+});
+
 function App() {
   return (
-    <main className="App">
-      <FleetDashboard />
-    </main>
+    <ThemeProvider theme={theme}>
+      <Container
+        // fixed
+        sx={
+          {
+            // mt: "40px",
+            // mb: "73px",
+            // ml: "52px",
+            // fontFamily: "poppins",
+          }
+        }
+      >
+        <FleetDashboard />
+      </Container>
+    </ThemeProvider>
   );
 }
 
