716fa5087e8b11e6d0fb9e69373cbfbf2e04d933
youjin-10 <105618795+youjin-10@users.noreply.github.com>
2024-07-13 23:05:37 +0900

    chore: more mock data for testing no robots associated

--- src/mocks/db.ts
+++ src/mocks/db.ts
@@ -4,7 +4,7 @@ export interface Location {
   robot: {
     id: string;
     is_online: boolean;
-  };
+  } | null;
 }
 
 export const locations: Location[] = [
@@ -21,6 +21,51 @@ export const locations: Location[] = [
     name: "Salty restaurant",
     robot: {
       id: "fghij456",
+      is_online: false,
+    },
+  },
+  {
+    id: 2,
+    name: "Sweet restaurant",
+    robot: null,
+  },
+  {
+    id: 3,
+    name: "pepper restaurant",
+    robot: {
+      id: "bbh12",
+      is_online: false,
+    },
+  },
+  {
+    id: 4,
+    name: "candy restaurant",
+    robot: {
+      id: "kjkj78",
+      is_online: true,
+    },
+  },
+  {
+    id: 5,
+    name: "mint restaurant",
+    robot: {
+      id: "yujhg56",
+      is_online: true,
+    },
+  },
+  {
+    id: 6,
+    name: "olive restaurant",
+    robot: {
+      id: "asdf890",
+      is_online: true,
+    },
+  },
+  {
+    id: 7,
+    name: "chocolate restaurant",
+    robot: {
+      id: "kkkmm6667",
       is_online: false,
     },
   },
