f1d0ff6520e35e290b62bf3dde0795769d76f15a
youjin-10 <105618795+youjin-10@users.noreply.github.com>
2024-07-13 18:14:30 +0900

    update: write Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,14 @@
 import React from "react";
+import { Typography } from "@mui/material";
 
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <Typography
+      variant="h5"
+      component="h1"
+      sx={{ fontWeight: 600, mb: "24px" }}
+    >
+      Your Fleet
+    </Typography>
+  );
 }
