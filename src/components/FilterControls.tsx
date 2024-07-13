0ab4629c552ab717c06f37b13b9bd06bb513dba3
youjin-10 <105618795+youjin-10@users.noreply.github.com>
2024-07-13 19:30:06 +0900

    update: write FilterControls component

--- src/components/FilterControls.tsx
+++ src/components/FilterControls.tsx
@@ -1,5 +1,49 @@
 import React from "react";
+import {
+  Box,
+  Select,
+  MenuItem,
+  TextField,
+  InputAdornment,
+} from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
 
-export default function FilterControls() {
-  return <div>FilterControls</div>;
+interface FilterControlsProps {
+  filter: "all" | "starred";
+  onFilterChange: (filter: "all" | "starred") => void;
+  searchQuery: string;
+  onSearchChange: (query: string) => void;
+}
+
+export default function FilterControls({
+  filter,
+  onFilterChange,
+  searchQuery,
+  onSearchChange,
+}: FilterControlsProps) {
+  return (
+    <Box sx={{ display: "flex", justifyContent: "space-between", mb: 2 }}>
+      <Select
+        value={filter}
+        onChange={(e) => onFilterChange(e.target.value as "all" | "starred")}
+        sx={{ minWidth: 150 }}
+      >
+        <MenuItem value="all">All Locations</MenuItem>
+        <MenuItem value="starred">Starred</MenuItem>
+      </Select>
+      <TextField
+        placeholder="Search robot ID or location"
+        value={searchQuery}
+        onChange={(e) => onSearchChange(e.target.value)}
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <SearchIcon />
+            </InputAdornment>
+          ),
+        }}
+        sx={{ minWidth: 300 }}
+      />
+    </Box>
+  );
 }
